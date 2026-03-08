0295f5f9b159c119cf75955738db7546d2161369
Bananka123321 <vanushabrend@gmail.com>
2026-03-08 16:02:38 +0700

    Fix caloriesService

--- DietDLL/src/CaloriesService.cpp
+++ DietDLL/src/CaloriesService.cpp
@@ -4,7 +4,7 @@
 double CaloriesService::CalculateTotalCalories(const Salad& salad) const {
     double count = 0.0;
     for (const auto& vegetable : salad.GetIngredients()){
-        count += vegetable->GetCalories();
+        count += vegetable->GetWeight();
     }
     return count;
 }
