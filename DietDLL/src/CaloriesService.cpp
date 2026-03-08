06f19e33c788333ede22f771d3dd4b0ee48f489b
Bananka123321 <vanushabrend@gmail.com>
2026-03-08 16:06:46 +0700

    Fix calories service counter

--- DietDLL/src/CaloriesService.cpp
+++ DietDLL/src/CaloriesService.cpp
@@ -4,7 +4,7 @@
 double CaloriesService::CalculateTotalCalories(const Salad& salad) const {
     double count = 0.0;
     for (const auto& vegetable : salad.GetIngredients()){
-        count += vegetable->GetWeight();
+        count += vegetable->GetCalories() * vegetable->GetWeight();
     }
     return count;
 }
